3d51ec780b0b6922a246ecc8c4ac8c88b731bdf0
sa0012 <1583479514@qq.com>
2019-01-24 23:44:25 +0800

    update

--- components/articleLabel/index.vue
+++ components/articleLabel/index.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="article-label">
-    <h3 class="label-title">
-      <div class="icon-wrap">
-        <i class="el-icon-menu"></i>
-      </div>
-      <span>文章标签</span>
-    </h3>
+    <h3 class="label-title">文章标签</h3>
     <ul class="label-list">
-      <li class="label-item" v-for="(label, index) in labelArr" :key="index">{{ label }}</li>
+      <li class="label-item" v-for="(label, index) in labelArr" :key="index">
+        <p class="label-text" @mousemove="showBackgroundColor">{{ label }}</p>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Colors from "~/plugins/color.js";
 export default {
   data() {
     return {
@@ -38,8 +36,12 @@ export default {
         "php模版 (35)",
         "正经事 (28)",
         "电子商务 (19)"
-      ]
+      ],
+      colorsArr: Colors.colorName
     };
+  },
+  methods: {
+    showBackgroundColor() {}
   }
 };
 </script>
@@ -49,5 +51,39 @@ export default {
   background: #fff;
   min-width: 400px;
   margin-top: 30px;
+  padding: 20px;
+  box-sizing: border-box;
+  transition: 0.5s ease;
+  &:hover {
+    border-radius: 4px;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);
+    transform: translateY(-5px);
+    transition: 0.5s ease;
+  }
+
+  .label-title {
+    font-size: 1pc;
+    font-weight: 400;
+    color: #333;
+    padding: 2px 0 2px 10px;
+    border-left: 2px solid #3472ef;
+    margin: 0 0 20px 2px;
+    height: 25px;
+    line-height: 25px;
+  }
+
+  .label-list {
+    .label-item {
+      display: inline-block;
+
+      .label-text {
+        padding: 5px 10px;
+        border-radius: 4px;
+        margin-right: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #ddd;
+      }
+    }
+  }
 }
 </style>
