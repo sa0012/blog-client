b348e707fd024409730f690be5876e1d9ab2bad0
chenlong <1583479514@qq.com>
2019-02-22 16:35:14 +0800

    update

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="foot">
-    <div class="police">
-      <img src="~/assets/image/beian.png">
-      <span>皖ICP备19002157号-1</span>
-    </div>
-    <div class="copyright">
-      <span>
-        Copyright © 2019&nbsp;
-        <a href="http://www.juckchen.cn">juckchen.cn &nbsp;</a>
-      </span>
-      <span>
-        Powered by Vue
-        <span>|</span>
-      </span>
-      <span>
-        <a href="http://localhost:8088/" target="_blank">&nbsp;管理员</a>
-      </span>
+    <div class="main">
+      <div class="police">
+        <img src="~/assets/image/beian.png">
+        <a href="http://www.beian.gov.cn/portal/index.do" target="_blank">皖ICP备19002157号-1</a>
+      </div>
+      <div class="copyright">
+        <span>
+          Copyright © 2019&nbsp;
+          <a href="http://www.juckchen.cn">juckchen.cn &nbsp;</a>
+        </span>
+        <span>
+          Powered by Vue
+          <span>|</span>
+        </span>
+        <span>
+          <a href="http://localhost:8088/" target="_blank">&nbsp;管理员</a>
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -39,8 +41,15 @@ export default {
   margin-top: -100px;
   height: 100px;
 
+  .main {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
   .police {
-    margin-bottom: 20px;
+    margin-bottom: 10px;
   }
 
   a {
